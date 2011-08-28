3af1525c4d69956aa82394932aba68ee45aa78f7
SenyaSay <zhenedyuk.z@ya.ru>
2011-08-28 15:59:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeBum::Application.config.secret_token = '663c38a59f733e99fcf69c119f72d03193c34e8ea0f8c125fc4d5ae74f5c2be011dcd5b3cd9e1cc791a6646002b6497ce9712c10b3d6062d66c6b771e01a9620'
