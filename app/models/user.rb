1a775f57bf682716417130c9035a8c76f7f0cabc
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2011-10-28 17:27:36 +0300

    Custom reg form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :token_authenticatable, :confirmable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+#  attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  include PhoneValidation
+#  include PhoneValidation
 end
