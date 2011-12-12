b8a62c0742dae5e7ff12e14b0ca00f3010cdeea4
Dordovsky Dmitry <dordovskydmitry@gmail.com>
2011-12-12 20:50:22 +0200

    Add breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def breadcrumbs(category)
+    if category
+      categories = category.ancestors.map(&:name)
+      categories.map do |c|
+        concat(link_to(c, catalog_url(:category => c)) + ' > ')
+      end
+      category.name
+    end
+  end
 end
