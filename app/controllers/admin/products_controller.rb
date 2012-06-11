0e67322e7ba2ff7c8e23628364535184cee812df
Drugnir <drugnir@drugnir-System-Product-Name.(none)>
2012-06-11 17:23:19 +0300

    Corrected admin_products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,7 +2,6 @@ class Admin::ProductsController < Admin::BaseController
   before_filter :product_find, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
     @products = Product.page(params[:page]).per(50)
   end
 
@@ -32,7 +31,7 @@ class Admin::ProductsController < Admin::BaseController
     if @product.update_attributes(params[:product])
       redirect_to :action => :index
     else
-      flash[:error] = @characteristic.errors.full_messages
+      flash[:error] = @product.errors.full_messages
       render :edit
     end 
   end
