bf65c7ef83c03f19ba7ea9b31dc296f8eb9b217c
dd <dmitriydordovskiy@anahoret.com>
2012-06-12 15:42:47 +0300

    Add test for menu

--- app/models/product_characteristic.rb
+++ app/models/product_characteristic.rb
@@ -1,5 +1,5 @@
 class ProductCharacteristic < ActiveRecord::Base
-  attr_accessible :characteristic_id, :value
+  attr_accessible :characteristic_id, :product_id, :value
   belongs_to :product
   belongs_to :characteristic
 end
