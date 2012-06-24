09168d1c91b1e91e551a19cb1b46f36809bd095a
Dordovskyi Dmitryi <dordovskydmitry@gmail.com>
2012-06-24 13:33:53 +0300

    add tect for products controller

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe ProductsController do
 
+  describe :index do
+    it "should be success" do
+      get :index
+      response.should be_success
+    end
+
+    it "should render index" do
+      get :index
+      response.should render_template(:index)
+    end
+
+    it "should set @products" do
+      get :index
+      assigns[:products].should_not be_nil
+    end
+  end
 end
