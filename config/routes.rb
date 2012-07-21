5ce0dd859cfc7ba6225cbc192360479153054965
Dordovskyi Dmitryi <dordovskydmitry@gmail.com>
2012-07-21 15:53:49 +0300

    Delete unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -58,8 +58,7 @@ OfficeBum::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'products#index'
-  resources :products
-  resources :characteristics
+  resources :products, :only => [:index, :show]
 
   # See how all your routes lay out with "rake routes"
 
