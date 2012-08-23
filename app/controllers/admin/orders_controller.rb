8e576e15dbf3a75693d739324dc628e4ed70448c
ArtemDordovsky <DordovskyArtem@gmail.com>
2012-08-23 18:13:25 +0300

    Add edit orders status for admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,27 @@
 class Admin::OrdersController < ApplicationController
+  before_filter :order_find, :only => [:edit, :update]
 
   def index
     @orders = Order.page(params[:page]).per(20)
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @order.update_attributes(params[:order])
+      redirect_to :action => :index
+    else
+      flash[:error] = @order.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def order_find
+    @order = Order.find(params[:id])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,8 @@ class Order < ActiveRecord::Base
   validates :status, :inclusion => [:reserved, :cancelled, :purchased, :deleted], :presence => true
   validates :user, :presence => true
 
+  STATUS = { :reserved => "reserved", :cancelled => "cancelled", :purchased=> "purchased", :deleted => "deleted" }
+
   def user_email(id)
     User.find_by_id(id).email
   end
